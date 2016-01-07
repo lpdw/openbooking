1f016d61649557d0b9efa8dd10a33b3c5b26eb04
Enguerran P <contact@epoulain.fr>
2016-01-07 18:34:14 +0100

    Fix wp install

--- dev_wp-install.sh
+++ dev_wp-install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #TODO : full auto (db config, etc...) if possible
-#mkdir test
+mkdir test
 cd test
-#curl https://fr.wordpress.org/wordpress-4.4-fr_FR.tar.gz -O
-#tar -xzvf wordpress-4.4-fr_FR.tar.gz
-#mv wordpress/* .
-#rm -Rf wordpress wordpress-4.4-fr_FR.tar.gz
-echo "Wordpress installé"
-#Ancienne méthode (copie)
-#cp -Rv ../openbooking wp-content/plugins
-#echo "Plugin copié"
+curl https://fr.wordpress.org/wordpress-4.4-fr_FR.tar.gz -O
+tar -xzvf wordpress-4.4-fr_FR.tar.gz
+mv wordpress/* .
+rm -Rf wordpress wordpress-4.4-fr_FR.tar.gz
+echo "Wordpress installï¿½"
+#Ancienne mï¿½thode (copie)
+cp -Rv ../openbooking wp-content/plugins
+echo "Plugin copiï¿½"
 cd ../
-echo "Création d'un lien symbolique (Windows)"
+echo "Crï¿½ation d'un lien symbolique (Windows)"
 ./dev_makesymlink_win.bat
-echo "Création d'un lien symbolique (Unix)"
+echo "Crï¿½ation d'un lien symbolique (Unix)"
 ./dev_makesymlink_unix.sh
-read -p "Appuyer sur [Entrée] pour quitter ..."
+read -p "Appuyer sur [Entrï¿½e] pour quitter ..."
